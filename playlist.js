4798c0bde2b9f1905c7ecc433ad647faebfe7f90
VickyPhu <victoriaphu@gmail.com>
2025-04-18 11:19:59 +0200

    added reference to html id

---
+++ playlist.js
@@ -0,0 +1,5 @@
+const playlists = [];
+
+const createPlaylistBtn = document.getElementById('create-playlist-btn');
+const playListContainer = document.getElementById('playlist-container');
+const playlistForm = document.getElementById('playlist-form');
