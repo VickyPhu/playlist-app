ca0eb9f7c355f8f35e94b77089990d6746a89cce
VickyPhu <victoriaphu@gmail.com>
2025-04-18 17:03:55 +0200

    render a form to create playlist when button is clicked

--- playlist.js
+++ playlist.js
@@ -3,3 +3,31 @@ const playlists = [];
 const createPlaylistBtn = document.getElementById('create-playlist-btn');
 const playListContainer = document.getElementById('playlist-container');
 const playlistForm = document.getElementById('playlist-form');
+
+createPlaylistBtn.addEventListener('click', () => {
+	showPlaylistForm();
+});
+
+function showPlaylistForm() {
+	playListContainer.innerHTML = '';
+
+	const form = document.createElement('form');
+
+	const input = document.createElement('input');
+	input.type = 'text';
+	input.placeholder = 'Playlist name';
+	input.required = true;
+
+	const createBtn = document.createElement('button');
+	createBtn.type = 'button';
+	createBtn.textContent = 'Create playlist';
+	// add click to add playlist function
+
+	const cancelBtn = document.createElement('button');
+	cancelBtn.type = 'button';
+	cancelBtn.textContent = 'Cancel';
+	// add click to cancel creation of a playlist
+
+	form.append(input, createBtn, cancelBtn);
+	playListContainer.appendChild(form);
+}
