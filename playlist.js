2e019ba48b9e5e1ff3c569c27740590f346f9497
VickyPhu <victoriaphu@gmail.com>
2025-05-15 22:47:44 +0200

    show songs in the select meny

--- playlist.js
+++ playlist.js
@@ -1,3 +1,7 @@
+const selectSong = document.getElementById('select-song');
+
+let songs = [];
+
 fetch('songs.json')
 	.then((response) => response.json())
 	.then((data) => {
@@ -6,4 +10,11 @@ fetch('songs.json')
 	})
 	.catch((error) => console.error('Error loading songs', error));
 
-
+function populateSelect(songs) {
+	songs.forEach((song, index) => {
+		const option = document.createElement('option');
+		option.value = index;
+		option.textContent = `${song.title} - ${song.artist}`
+		selectSong.appendChild(option)
+	})
+}
