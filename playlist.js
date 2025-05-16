c1a2f1cf0538dc6b4af93b89371adef3d9f45d1d
VickyPhu <victoriaphu@gmail.com>
2025-05-16 20:44:15 +0200

    changed to checkboxes

--- playlist.js
+++ playlist.js
@@ -1,20 +1,32 @@
-const selectSong = document.getElementById('select-song');
+const form = document.getElementById('playlist-form');
+const songCheckboxes = document.getElementById('song-checkboxes');
+const cancelBtn = document.getElementById('cancel-btn');
 
+const playlists = [];
 let songs = [];
 
 fetch('songs.json')
 	.then((response) => response.json())
 	.then((data) => {
 		songs = data.songs;
-		populateSelect(songs);
+		populateCheckboxes(songs);
 	})
 	.catch((error) => console.error('Error loading songs', error));
 
-function populateSelect(songs) {
+function populateCheckboxes(songs) {
 	songs.forEach((song, index) => {
-		const option = document.createElement('option');
-		option.value = index;
-		option.textContent = `${song.title} - ${song.artist}`
-		selectSong.appendChild(option)
-	})
+		const label = document.createElement('label');
+		label.style.display = 'block';
+
+		const checkbox = document.createElement('input');
+		checkbox.type = 'checkbox';
+		checkbox.value = index;
+		checkbox.name = 'song';
+
+		label.appendChild(checkbox);
+		label.appendChild(
+			document.createTextNode(`${song.title} - ${song.artist}`)
+		);
+		songCheckboxes.appendChild(label);
+	});
 }
